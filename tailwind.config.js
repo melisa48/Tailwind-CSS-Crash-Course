2cf28c317ff4408d3d1dc2053a31cf45768543da
melisa48 <60372223+melisa48@users.noreply.github.com>
2024-04-25 15:04:44 -0700

    Project is done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    container:{
+      center: true,
+      padding: '2rem'
+    }
+  },
+  plugins: [],
+}
+
